f4fd2db79f3f4b7076bcb957bda9278bd04273d5
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2016-01-23 16:16:50 -0500

    organize aliases and add a few more

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,22 +1,31 @@
 # Aliases
 
-alias g="git"
+# override vi/vim with nvim
 alias vi="nvim"
 alias vim="nvim"
-alias t="tmux"
-alias h="history"
+
+# simple
+alias ..="cd .."
 alias lsl="ls -a"
 alias mkdir="mkdir -p"
-alias ..="cd .."
+
+# shortenings
+alias g="git"
+alias t="tmux"
+alias h="history"
 alias pa="php artisan"
 alias cdumrs="composer dump-autoload && php artisan migrate:refresh --seed"
 
-# Start simple python web server in cwd on port 7777
+# edit certain files
+alias e_v="vim ~/.config/nvim/init.vim"
+alias e_z="vim ~/.zshrc"
+
+# start simple python web server in cwd on port 7777
 alias serve="python3 -m http.server 7777"
-# or on another port
+# ...or on another port
 alias serveon="python3 -m http.server"
 
-# mkdir && cd
+# mkdir, then cd
 mcd()
 {
     if [ ${1} ]
