efebe217ef98395660a326a12ba5845c7972b9ae
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2019-05-31 13:25:59 -0400

    fo fzf function include hidden files

--- scripts/zsh/functions.sh
+++ scripts/zsh/functions.sh
@@ -18,7 +18,7 @@ mcd()
 #   - CTRL-E or Enter key to open with the $EDITOR
 fo() {
 	local out file key
-	IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+	IFS=$'\n' out=($(FZF_DEFAULT_COMMAND="ag --hidden --ignore .git -g \"\"" fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
 	key=$(head -1 <<< "$out")
 	file=$(head -2 <<< "$out" | tail -1)
 	if [ -n "$file" ]; then
